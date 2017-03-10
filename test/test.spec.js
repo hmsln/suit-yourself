a13a26a1cccf09794ccb6218e02c356c2000c562
Hubert <hubert.maslin@gmail.com>
2017-03-10 23:57:06 +0100

    corrected tab length

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,32 +1,31 @@
 'use strict';
 
-var suitYourself = require('../src/suit-yourself.js');
-
-var suitYourself = require('../src/suit-yourself.js');
-
-var path = __dirname + '/test-submodules';
-
-var availableSubmodules = {
-	'submodule1': {},
-    'submodule2': {
-    	isFactory: true
-    }
-};
-
-var argument = 'argument';
-
-var submodules = suitYourself(path, availableSubmodules, [
-    {
-       name: 'submodule1'
-    },
-	{
-        name: 'submodule2',
-        args: argument
-    }
-]);
-
 describe('suit yourself', function () {
 	
+  	var suitYourself = require('../src/suit-yourself.js');
+
+  	var path = __dirname + '/test-submodules';
+
+  	var availableSubmodules = {
+    	'submodule1': {},
+      	'submodule2': {
+        	isFactory: true //requiring submodule2 returns a factory thhat can be called to return the actual submodule
+      	}
+  	};
+  
+  	//test argument that's going to be passed to submodule2
+  	var argument = 'argument';
+  
+  	//get submodules
+  	var submodules = suitYourself(path, availableSubmodules, [{
+        	name: 'submodule1'
+    	},
+    	{
+        	name: 'submodule2',
+        	args: argument
+    	}
+    ]);
+  
 	it('loads specified submodule', function () {
         expect(submodules.submodule1('argument')).toEqual('argument');
     });
